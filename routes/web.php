2704e21da0b61422235cc192db16cc3ec572324c
deniseasc <denise1526alves@gmail.com>
2019-10-03 17:49:30 -0300

    ROUTE

--- routes/web.php
+++ routes/web.php
@@ -11,14 +11,18 @@
 |
 */
 
-Route::get('/rota1', function () {
-    return '<h1> Esta é a rota 1 </h1>';
-});
+Route::group(["prefix"=>"ex1"], function(){
 
-Route::get('/rota2', function () {
-    return '<h1> Esta é a rota 2 </h1>';
-});
+    Route::get('rota1', function () {
+        return '<h1> Esta é a rota 1 </h1> <a href="./rota2"> <button> Próxima Rota</button>';
+    });
+    
+    Route::get('rota2', function () {
+        return '<h1> Esta é a rota 2 </h1> <a href="./rota3"> <button> Próxima Rota</button>';
+    });
+    
+    Route::get('rota3', function () {
+        return '<h1> Esta é a rota 3 </h1> <a href="./rota1"> <button> Voltar </button>';
+    });
 
-Route::get('/rota3', function () {
-    return '<h1> Esta é a rota 3 </h1>';
 });
